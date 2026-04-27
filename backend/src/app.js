8b682cbddec48afc3938f60b423cbfc8831ef06c
Dakakpo7 <keziaakakpo2001@gmail.com>
2026-04-27 12:02:39 +0000

    CORS - autoriser le domaine Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://sanovita-xxxx.vercel.app'
+    'https://sanovita.vercel.app/'
   ],
   credentials: true
 }));
